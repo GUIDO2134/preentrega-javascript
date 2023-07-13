42dee00fec1cfc12318fa66f6139253fb82974a0
guido riva <guidoriva112@gmail.com>
2023-07-12 22:14:20 -0300

    js

--- js/main.js
+++ js/main.js
@@ -9,16 +9,14 @@
 
     ciclos ESTA
     for
-    while
+    while ESTA
 
     funciones
        funciones propias de java ESTA
        funciuones cradas pro nosotros ESTA
 */
-// Función para comparar el número ingresado por el jugador con el número secreto
 // Generar un número aleatorio entre 1 y 100
 const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-
 // Función principal del juego
 function jugarAdivinanza() {
   let intentos = 0;
